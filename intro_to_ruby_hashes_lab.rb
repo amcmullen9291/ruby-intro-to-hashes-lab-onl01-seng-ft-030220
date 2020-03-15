1a3dbcf247edbc12ff43bac551eaeb82e1b28707
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-15 02:14:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,9 +18,18 @@ monopoly = {
 	    :names => {},
 	    :rent_in_dollars => {}
 	  }
-	}end
+	}
+	end
 
 def monopoly_with_third_tier
+  monopoly = {
+	  :railroads => {
+	    :pieces => 4,
+	    :names => {},
+	    :rent_in_dollars => 25
+	  }
+	}
+
 end
 
 def monopoly_with_fourth_tier
